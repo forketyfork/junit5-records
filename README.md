70f3155858ab07886328e066b94314e47f0a3375
Sergei Petunin <petunin@crxmarkets.com>
2020-04-21 08:54:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# junit5-records
+A demo of JUnit 5 parameterized tests using Java 14 records.
---
+++ src/main/java/com/forketyfork/junit5records/Problem.java
@@ -0,0 +1,25 @@
+package com.forketyfork.junit5records;
+
+import java.util.HashSet;
+
+/**
+ * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+ * <p>
+ * For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+ * <p>
+ * Bonus: Can you do this in one pass?
+ */
+public class Problem {
+
+    public boolean check(int target, int[] array) {
+        var seen = new HashSet<Integer>();
+        for (int value : array) {
+            if (seen.contains(target - value)) {
+                return true;
+            }
+            seen.add(value);
+        }
+        return false;
+    }
+
+}
